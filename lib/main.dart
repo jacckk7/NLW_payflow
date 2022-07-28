811b862033cdef5daf733a101976082ff37c93a5
Breno <brenocal73@gmail.com>
2022-07-28 16:29:15 -0300

    feat: Feita integração com Firebase e a página home.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
-import 'modules/login/login_page.dart';
-import 'package:payflow/modules/home/home_page.dart';
+import 'app_widget.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
+class AppFirebase extends StatefulWidget {
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: HomePage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        //Check for errors
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                "Não foi possível inicializar o Firebase",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        }
+
+        //Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        }
+
+        //Otherwise, show something whilst waiting
+        return Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+      },
     );
   }
 }
--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 
+import '../../shared/auth/auth_controller.dart';
+
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final authController = AuthController();
+    authController.currentUser(context);
+
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
